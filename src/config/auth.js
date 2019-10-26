21d2f8b18060280fdc412520e312b1938fbf5019
Edison <mage1232010@gmail.com>
2019-10-26 00:19:51 -0300

    Initial Commit

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'f29618255c309de4469993cce24286ea',
+  expireIn: '7d'
+};
---
+++ src/database/index.js
@@ -0,0 +1,25 @@
+import Sequelize from 'sequelize';
+
+import User from '../app/models/User';
+import File from '../app/models/File';
+import Appointment from '../app/models/Appointment';
+
+import databaseConfig from '../config/database';
+
+const models = [User, File, Appointment];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.connection = new Sequelize(databaseConfig);
+
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
+  }
+}
+
+export default new Database();
