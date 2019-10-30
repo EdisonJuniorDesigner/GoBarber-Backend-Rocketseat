7d8a252183ef7e9cf8d7ba76ad0179b983d398e6
Edison <mage1232010@gmail.com>
2019-10-30 00:50:37 -0300

    Adicionado função delete do Appointment e Mail

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '9428caa5892213',
+    pass: '0cec2f64f8e6b4'
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>'
+  }
+};
