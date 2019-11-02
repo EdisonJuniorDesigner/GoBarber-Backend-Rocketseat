15deab85e47a4d0a9d129b8302fb13542617c308
Edison <mage1232010@gmail.com>
2019-11-02 02:02:20 -0300

    Add variáveis ambientes (env), Env.Example

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '9428caa5892213',
-    pass: '0cec2f64f8e6b4'
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>'
